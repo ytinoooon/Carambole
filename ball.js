bfbca7f62eb81bb1acc85256fc38c41e53e43377
tesnadi tamas <tesnaditamas@Apaczai.org>
2021-09-28 13:37:16 +0300

    accalaration fixed and now accelerates and than it deaccalarates

--- ball.js
+++ ball.js
@@ -1,11 +1,14 @@
 let xgoingback = false;
 let ygoingback = false;
 class ball{
-    constructor(x,y,d,xs,ys) {
+    constructor(x,y,d) {
         this.pos = createVector(x,y);
         this.d = d;
-        this.speed = createVector(xs,ys);
-        this.acc = -0.1; 
+        this.speed = createVector(10,25);
+        this.acc = 0; 
+        this.acc_changer = 0.1;
+        this.c = ["green","white","red"];
+        this.actual = this.c[1];
     }
     go(op) {
         if(this.speed.x == 0 | this.speed.y == 0) {
@@ -32,33 +35,50 @@ class ball{
     }
 
     update() {
-        if(this.pos.x >= width) {
-            xgoingback = true;
-        } else if(this.pos.y >= height) {
-            ygoingback = true;
-        }
-        if(this.pos.x <= 0) {
-            xgoingback = false;
-        } else if(this.pos.y <= 0) {
-            ygoingback = false;
-            this.c = random("green","purple","red");
-        }
+        if(this.speed.y <= 0 | this.speed.x <= 0 ) {
+           // background(51);
+            this.acc = 0;
+            this.speed.x = 0; 
+            this.speed.y = 0;
+            //this.pos.x = 300;
+            //this.pos.y = 300;
+            console.log(this.speed.x);
+            console.log(this.speed.y);
+            this.show();
+        } else {
+            if(this.pos.x >= width) {
+                xgoingback = true;
+            } else if(this.pos.y >= height) {
+                ygoingback = true;
+            }
+            if(this.pos.x <= 0) {
+                xgoingback = false;
+            } else if(this.pos.y <= 0) {
+                ygoingback = false;
+            }
+    
+            if(xgoingback){
+                this.go("x-");
+            } else {
+                this.go("x+");
+            }
+            if(ygoingback) {
+                this.go("y-");
+            } else {
+                this.go("y+");
+            }
 
-        if(xgoingback){
-            this.go("x-");
-        } else {
-            this.go("x+");
-        }
-        if(ygoingback) {
-            this.go("y-");
-        } else {
-            this.go("y+");
-        }
+            this.acc = this.acc + this.acc_changer;
+            if(this.acc >= 1.5){
+                this.acc_changer = -0.1;
+            }
+            
+        }       
     }
 
     show() {
         noStroke();
-        fill(255);
+        fill(this.actual);
         circle(this.pos.x,this.pos.y,this.d);        
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,15 +1,11 @@
 let golyo;
 function setup() {
     createCanvas(600,600);
-    for(let i = 0; i < 1 ; i++) {
-        golyo[i] = new ball(width/2,height/2,20,random(1,50),random(1,50));
-    }
+    golyo = new ball(width/2,height/2,20);
 }
 
 function draw() {
     background(61);
-    for(let i = 0; i < 1 ; i++) {
-        golyo.show();
-        golyo.update();
-  }
+    golyo.show();
+    golyo.update();
 }
